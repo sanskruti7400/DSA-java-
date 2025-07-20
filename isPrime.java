e21bb8372bbbc11b56a0dd95215bddbfd84ec904
Sanskruti Dhanaji Kadam <sanskadam7400@gmail.com>
2025-07-20 09:24:29 -0700

    Create isPrime.java

---
+++ isPrime.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPrime(int n) {
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
